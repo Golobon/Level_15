d6e592d6819cce2d22c215456d741d312404432a
Golobon <golobon@ya.ru>
2022-09-14 23:41:42 +0300

    Commit

--- src/Lecture_6/Solution_3.java
+++ src/Lecture_6/Solution_3.java
@@ -1,11 +1,13 @@
 package Lecture_6;
 
+import java.util.HashMap;
+
 public class Solution_3 {
     public static void main(String[] args) {
         try {
-            int[] array = new int[2];
-            array[8] = 5;
-        } catch (ArrayIndexOutOfBoundsException e) {
+            HashMap<String, String> map = new HashMap<>(null);
+            System.out.println(map);
+        } catch (NullPointerException e) {
             System.out.println(e);
         }
     }
