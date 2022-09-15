bd9b549e9b8171cce0a258c37c14fe59f0a3f4f8
Golobon <golobon@ya.ru>
2022-09-15 18:50:08 +0300

    Commit

---
+++ src/Lecture_8/Solution_6.java
@@ -0,0 +1,20 @@
+package Lecture_8;
+
+import java.io.IOException;
+
+public class Solution_6 {
+    public static void main(String[] args) {
+    }
+
+    static class MyException extends RuntimeException {
+    }
+
+    static class MyException2 extends RuntimeException {
+    }
+
+    static class MyException3 extends IOException {
+    }
+
+    static class MyException4 extends IOException {
+    }
+}
